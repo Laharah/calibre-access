3bc8cb4618b145ccfacd12e50f484ee36cb2259f
laharah <laharah22@gmail.com>
2015-03-03 12:05:30 -0800

    records are now collected via generator

--- lookup_ips.py
+++ lookup_ips.py
@@ -12,49 +12,45 @@ DownloadRecord = namedtuple("DownloadRecord",
 def load_record_strings():
     with open("server_access_log.txt") as f:
         data = f.readlines()
-    records = []
     for line in data:
-        match = re.search(r'.*(\.mobi|\.epub).*', line)
+        match = re.search(r'.*(\.mobi|\.epub|\.azw).*', line)
         if match:
-            records.append(match.group())
+            yield match.group()
+    del data
 
-    return records
 
-
-def parse_records(records):
-    downloads = []
+def parse_record(record):
     pattern = r'^(\d+\.\d+\.\d+\.\d+).*\[(.*)\].*GET /get/\w+/(.+?\.\w{3,5}) '
-    for line in records:
-        match = re.search(pattern, line)
-        if match:
-            downloads.append(match)
-    return translate_matches(downloads)
+    match = re.search(pattern, record)
+    if match:
+        return translate_match(match)
 
 
-def translate_matches(matches):
-    downloads = []
+def translate_match(match):
     gi = pygeoip.GeoIP('GeoLiteCity.dat')
-    for match in matches:
-        loc = gi.record_by_addr(match.group(1))
-        if not loc:
-            downloads.append(match.groups())
-            continue
-        try:
-            loc_string = ', '.join([loc['city'], loc['region_code']])
-        except TypeError:
-            loc_string = loc['metro_code']
-        downloads.append(
-            DownloadRecord(match.group(1), match.group(2), loc_string,
-                           match.group(3)))
-    return downloads
+    loc = gi.record_by_addr(match.group(1))
+    if not loc:
+        loc = {'city': "NONE", 'region_code': "NONE"}
+    try:
+        loc_string = ', '.join([loc['city'], loc['region_code']])
+    except TypeError:
+        loc_string = loc['country_name']
+
+    download = DownloadRecord(match.group(1), match.group(2), loc_string,
+                              match.group(3))
+    return download
 
 
 def main():
     records = load_record_strings()
-    downloads = parse_records(records)
-    for dl in downloads:
-        print dl
+    download_records = []
+    for record in records:
+        download_record = parse_record(record)
+        print download_record
+        download_records.append(download_record)
 
+    print "total downloads: {}".format(len(download_records))
+    print "unique ips: {}".format(len({r.ip for r in download_records}))
 
 if __name__ == '__main__':
     main()
