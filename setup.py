3bef7000ff12eed993c03dd69287fb493c40927e
laharah <laharah22@gmail.com>
2016-02-12 16:08:17 -0800

    - added support for `setup.py test`

--- setup.py
+++ setup.py
@@ -9,10 +9,16 @@ setup(
     author_email='laharah22+ca@gmail.com',
     description='Quick and dirty log parser for calibre server',
     py_modules=['calibre_access', 'utilities'],
+    setup_requires=['pytest-runner'],
     install_requires=[
         'pygeoip >= 0.3',
         'appdirs >= 1.4.0',
-        'docopt >= 0.5.0'
+        'docopt >= 0.5.0',
+    ],
+    tests_require=[
+        'pytest >= 2.8.7',
+        'mock >= 1.3.0',
+        'httpretty >= 0.8.14',
     ],
     entry_points={
         'console_scripts': [
