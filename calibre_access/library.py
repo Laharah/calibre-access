4dde360d390a6f84ace6918935ddfa2e5cafd266
laharah <laharah22@gmail.com>
2017-08-05 01:17:02 -0700

    added BookLibrary Context manager

--- calibre_access/library.py
+++ calibre_access/library.py
@@ -1,5 +1,31 @@
 import sqlite3
 
-def get_cursor(file):
-    con = sqlite3.connect(file)
-    return con.cursor()
+
+class BookLibrary(object):
+    """a context wrapper for calibre-db lookups"""
+
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.con = None
+        self.cur = None
+
+    def __enter__(self):
+        try:
+            self.con = sqlite3.connect('file:{}?mode=ro'.format(self.db_file), uri=True)
+        except TypeError:
+            self.con = sqlite3.connect(self.db_file)  # URI only supported py >= 3.4
+
+        self.cur = self.con.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.cur.close()
+        self.con.close()
+
+    def __getitem__(self, index):
+        assert self.con is not None and self.cur is not None
+        self.cur.execute('SELECT title FROM books WHERE id = ?', (index, ))
+        try:
+            return self.cur.fetchone()[0]
+        except TypeError:
+            raise KeyError(str(index))
--- test/test_library.py
+++ test/test_library.py
@@ -3,9 +3,12 @@ import pytest
 import os
 import tempfile
 import shutil
+import hashlib
+import sys
 
 from mock_sql import SQL
 import calibre_access.library as lb
+
 
 @pytest.yield_fixture(scope='module')
 def mock_db_file():
@@ -22,6 +25,36 @@ def mock_db_file():
         shutil.rmtree(td)
 
 
-def test_get_cursor(mock_db_file):
-    c = lb.get_cursor(mock_db_file)
-    assert c.execute("SELECT title from books where id = 1").fetchone() == ('Book One',)
+def test_book_library_instantiation(mock_db_file):
+    bl = lb.BookLibrary(mock_db_file)
+
+
+def test_book_library_lookup(mock_db_file):
+    with lb.BookLibrary(mock_db_file) as bl:
+        assert bl['1'] == "Book One"
+        assert bl['2'] == "Book Two"
+        assert bl['4'] == "This is a Very Long Book Name That Will Be Truncated"
+
+
+def test_book_library_lookup_must_be_context(mock_db_file):
+    bl = lb.BookLibrary(mock_db_file)
+    with pytest.raises(AssertionError):
+        t = bl['1']
+
+
+@pytest.mark.skipif(sys.version_info < (3, 4), reason='unsuported feature py < 3.4')
+def test_assure_no_write(mock_db_file):
+    with open(mock_db_file, 'rb') as f:
+        h = hashlib.sha1(f.read()).hexdigest()
+    with lb.BookLibrary(mock_db_file) as bl:
+        with pytest.raises(sqlite3.OperationalError):
+            bl.cur.execute('DROP TABLE books')
+            bl.con.commit()
+    with open(mock_db_file, 'rb') as f:
+        assert hashlib.sha1(f.read()).hexdigest() == h
+
+
+def test_bad_index_raises_key_error(mock_db_file):
+    with lb.BookLibrary(mock_db_file) as bl:
+        with pytest.raises(KeyError):
+            bl['10'] == None
